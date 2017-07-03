6a3129dd26119d16e4b11b50cf2193f827fbeb7e
Nair <abhnai01@protiviti.com>
2017-07-03 17:37:46 -0400

    updated app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //can configure to use promises that are built in
 //mongoose handles connection prior to scheduling any queries
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.export = {mongoose};
