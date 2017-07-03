ce0988a307c9207e32428e2aa9eac6bf86771dd5
Nair <abhnai01@protiviti.com>
2017-07-03 15:13:18 -0400

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise; //can configure to use promises that are built in
 //mongoose handles connection prior to scheduling any queries
 
-mongoose.connect(process.env.MONGODB_URI, || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
 module.export = {mongoose};
