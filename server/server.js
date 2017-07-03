5a9890f5388fa671c95885811fa0697dba8b91f2
Nair <abhnai01@protiviti.com>
2017-07-03 14:23:37 -0400

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');//destructuring require
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -32,6 +32,32 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//Get request/todos/id
+app.get('/todos/:id', (req, res) => {
+  //key -value: url param-value of url param-value
+  var id = req.params.id;
+
+  //validate Id using isValud
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  //find by ID
+    //sucess case - if todo exists send it back and if no todo send back 404 with empty body
+    //error case - send back 400
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+      res.send({todo});
+
+  }).catch((e) => res.status(400).send());
+
+
+
+
 });
 
 app.listen(3000, () => {
